dda12f9efdb4d61660ddf6a5ac7a8a46e5e5bca5
Jiaming Ding <e1351662@u.nus.edu>
2024-11-08 15:16:27 +0800

    add main file

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("DSS5201 Data Visualization Group Project")
+st.header("Welcome to our dashboard!")
+st.write(
+    "In this dashboard, we will reproduce the graphs from web and explore the social media usage in the US."
+)
+
+st.subheader("Group Members (in alphametic order):")
+st.write("Ding Jiaming, Li Jingming, Niu Muyuan, Wang Hao, Zhang Yi")
