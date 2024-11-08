aed8c90c17096ec394d5aecd37931ef9fcf5ad1b
Jiaming Ding <e1351662@u.nus.edu>
2024-11-08 14:56:23 +0800

    rename the data and try to read from github

--- pages/02_Social_Media_Usage_in_US.py
+++ pages/02_Social_Media_Usage_in_US.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests
+import openpyxl
 from io import StringIO
 
 
@@ -16,18 +17,18 @@ from io import StringIO
 #################################################### Original Data ####################################################
 
 ## % of U.S. adults who say they ever use ... by ...
-us_usage_by_group_url = "https://github.com/heyyyjiaming/DSS5201-SocialMediaUsage-Dashboard/blob/main/data/Social%20Media%20Usage_pivoted.xlsx"
-# us_usage_by_group = pd.read_excel("../data/original/Social Media Usage_pivoted.xlsx", header=0)
-response1 = requests.get(us_usage_by_group_url)
-if response1.status_code == 200:
-    us_usage_by_group = pd.read_excel(StringIO(response1.text), header=0)
-else:
-    st.error("Failed to load data from GitHub.")
+# us_usage_by_group_url = "https://raw.githubusercontent.com/heyyyjiaming/DSS5201-SocialMediaUsage-Dashboard/refs/heads/main/data/Social%20Media%20Usage_pivoted.xlsx"
+# # us_usage_by_group = pd.read_excel("../data/original/Social Media Usage_pivoted.xlsx", header=0)
+# response1 = requests.get(us_usage_by_group_url)
+# if response1.status_code == 200:
+#     us_usage_by_group = pd.read_excel(StringIO(response1.text), header=0)
+# else:
+#     st.error("Failed to load data from GitHub.")
 # us_usage_by_group = load_original_data(us_usage_by_group_url)
 
 
 ## % of U.S. adults who say they ever use … (Polls from 2012-2021)
-us_popular_platforms_url
+us_popular_platforms_url = "https://raw.githubusercontent.com/heyyyjiaming/DSS5201-SocialMediaUsage-Dashboard/refs/heads/main/data/which_social_media_platforms_are_most_popular_data_2024-01-31.csv"
 response2 = requests.get(us_popular_platforms_url)
 if response2.status_code == 200:
     us_popular_platforms = pd.read_csv(StringIO(response2.text), skiprows=3, delimiter=',', header=0)
@@ -63,12 +64,12 @@ if ori_on:
     with col1:
         dim_option = st.selectbox("Please select a dimension you want to check",
                                     ('None', 'Age', 'Gender', 'Income', 'Political Affiliation', 'Race & Ethnicity'))
-        if dim_option == 'None':
-            st.markdown(f"% of U.S. adults who say they ever use ... by ...")
-            st.dataframe(us_usage_by_group)
-        else:
-            st.markdown(f"% of U.S. adults who say they ever use ... by {dim_option}")
-            st.dataframe(us_usage_by_group[us_usage_by_group['Dimension'] == dim_option])
+        # if dim_option == 'None':
+        #     st.markdown(f"% of U.S. adults who say they ever use ... by ...")
+        #     st.dataframe(us_usage_by_group)
+        # else:
+        #     st.markdown(f"% of U.S. adults who say they ever use ... by {dim_option}")
+        #     st.dataframe(us_usage_by_group[us_usage_by_group['Dimension'] == dim_option])
 
     with col2:
         placeholder(5)
